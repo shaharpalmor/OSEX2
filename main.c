8de3352e59105b6af00cd4fa27e74fbb34348551
shaharpalmor <shaharpalmor@gmail.com>
2018-04-02 00:26:37 +0300

    changing so there would be only one function for commands

--- main.c
+++ main.c
@@ -8,85 +8,96 @@
 
 #define COMMAND_LENGTH 42
 
+void makeArgumentsArray(char *command, char *argv[COMMAND_LENGTH]) {
+    int i = 0;
+    char *split = strtok(command, " ");
+    //split = strtok(NULL," ");
+    while (split != NULL) {
+        if (strcmp(split, "&") != 0) {
+            argv[i] = split;
+        }
+        split = strtok(NULL, " ");
+        i++;
+    }
+    argv[i] = NULL;
+}
 
-int lsCommand(char *command){
+int lsCommand(char *command) {
     int i = 0;
     int j = 0;
     char *argv[COMMAND_LENGTH];// moving the arguments to an array
     char *argvCopy[COMMAND_LENGTH];// changing the order of the arguments - swaping
     pid_t result = fork();
 
-    if(result == 0){ //the sons process
-        char *split = strtok(command," ");
-        split = strtok(NULL," ");
-        while(split != NULL)
-        {
-            if(strcmp(split, "&") != 0)
-                    argv[i] = split;
-            split = strtok(NULL," ");
+    if (result == 0) { //the sons process
+        char *split = strtok(command, " ");
+        split = strtok(NULL, " ");
+        while (split != NULL) {
+            if (strcmp(split, "&") != 0)
+                argv[i] = split;
+            split = strtok(NULL, " ");
             i++;
         }
         i--;
 
-        for (j = 0; j <= i; j++){
+        for (j = 0; j <= i; j++) {
             argvCopy[j] = argv[i];
             i--;
         }
         argvCopy[j] = argv[i];
 
-        execvp("ls",argvCopy);
+        execvp("ls", argvCopy);
 
-    } else if (result ==-1){
+    } else if (result == -1) {
         printf("could not create process for the ls command");
     } else {// father process
         printf("%d\n", result);
-        if(isAmpersent(command)) // the father has no need to wait for the sons process
+        if (isAmpersent(command)) // the father has no need to wait for the sons process
             return result;
         else
-           waitpid(result, NULL, 0);
+            waitpid(result, NULL, 0);
     }
     return result;
 }
 
-int isAmpersent(char *command){
+int isAmpersent(char *command) {
 
     char *argv[COMMAND_LENGTH];
     char cmd[COMMAND_LENGTH];
-    strcpy(cmd,command);
-    char *split = strtok(cmd," ");
-    split = strtok(NULL," ");
+    strcpy(cmd, command);
+    char *split = strtok(cmd, " ");
+    split = strtok(NULL, " ");
     int i = 1;
 
-    while(split != NULL)
-    {
+    while (split != NULL) {
         argv[i] = split;
-        split = strtok(NULL," ");
+        split = strtok(NULL, " ");
         i++;
     }
-    if (strcmp(argv[i-1],"&") == 0){
+    if (strcmp(argv[i - 1], "&") == 0) {
         return 1;// means that the father process should run and not wait for the son
     }
     return 0;// means that the father process should not run and wait for the son
 
 }
 
-int catCommand(char *command){
+int execCommand(char *command) {
     int j = 0;
     char *argv[COMMAND_LENGTH];// moving the arguments to an array
     pid_t result = fork();
-    if(result == 0){ //the sons process
-        makeArgumentsArray(command,argv);
-        j = execvp(argv[0],argv);
-        if (j == -1){
+    if (result == 0) { //the sons process
+        makeArgumentsArray(command, argv);
+        j = execvp(argv[0], argv);
+        if (j == -1) {
             fprintf(stderr, "Failed to execute %s\n", argv[0]);
             perror("execvp");
-            exit (EXIT_FAILURE);
+            exit(EXIT_FAILURE);
         }
-    } else if (result ==-1){
+    } else if (result == -1) {
         printf("could not create process for the ls command");
     } else {// father process
         printf("%d\n", result);
-        if(isAmpersent(command)) // the father has no need to wait for the sons process
+        if (isAmpersent(command)) // the father has no need to wait for the sons process
             return result;
         else
             waitpid(result, NULL, 0);
@@ -94,45 +105,8 @@ int catCommand(char *command){
     return result;
 }
 
-int sleepCommand(char *command){
-    int j = 0;
-    char *argv[COMMAND_LENGTH];// moving the arguments to an array
-    pid_t result = fork();
 
-    if(result == 0){ //the sons process
-        makeArgumentsArray(command,argv);
-        j = execvp(argv[0],argv);
-        if (j == -1){
-            fprintf(stderr, "Failed to execute %s\n", argv[0]);
-            perror("execvp");
-            exit (EXIT_FAILURE);
-        }
-    } else if (result ==-1){
-        printf("could not create process for the ls command");
-    } else {// father process
-        printf("%d\n", result);
-        if(isAmpersent(command)) // the father has no need to wait for the sons process
-            return result;
-        else
-            waitpid(result, NULL, 0);
-    }
-    return result;
-}
 
-void makeArgumentsArray(char *command, char *argv[COMMAND_LENGTH]){
-    int i = 0;
-    char *split = strtok(command," ");
-    //split = strtok(NULL," ");
-    while(split != NULL)
-    {
-        if(strcmp(split, "&") != 0) {
-            argv[i] = split;
-        }
-        split = strtok(NULL," ");
-        i++;
-    }
-    argv[i] = NULL;
-}
 
 int main() {
 
@@ -140,32 +114,32 @@ int main() {
     char line[COMMAND_LENGTH];
     char cmd[COMMAND_LENGTH];
     pid_t pid; // the process that is currently running.
+    char *jobsArray[COMMAND_LENGTH];
+    int pidArray[COMMAND_LENGTH];
+    int i = 0;
+    int j;
 
     while (1) {
-
         printf("%s", prompt);
         fgets(line, COMMAND_LENGTH, stdin);
-        line[strlen(line)-1]='\0';
-        strcpy(cmd,line);
+        line[strlen(line) - 1] = '\0';
+        strcpy(cmd, line);
         strtok(cmd, " ");
 
-        if (strcmp(cmd,"ls") == 0){
-            pid = lsCommand(line);
-        } else if(strcmp(cmd,"cat") == 0){
-            pid = catCommand(line);
-        } else if(strcmp(cmd,"sleep") == 0){
-            pid = catCommand(line);
-        } else if(strcmp(cmd,"cd") == 0){
-
-        } else if (strcmp(cmd,"jobs") == 0){
-
-        } else if(strcmp(cmd,"exit") == 0)
+        if (strcmp(cmd, "exit") == 0) {
             break;
-        else
-            continue;
-
+        } else if (!strcmp(cmd, "jobs") == 0) {
+            pid = execCommand(line);
+            pidArray[i] = pid;
+            jobsArray[i] = cmd;
+        } else { // jobs command was asked.
+            for (j = 0; j < i; j++) {
+                pid_t pidProcess = waitpid(pidArray[i], NULL, WNOHANG);
+                if (pidProcess == 0)
+                    printf("%d %s", pidArray[i], jobsArray[i]);
+            }
+        }
     }
-
     return 0;
 }
 
